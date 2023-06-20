0e624c0f48d8a2dc0becfedb94046ec1a82b13bc
Ericksulino <ericksulino@gmail.com>
2023-06-20 19:30:28 -0300

    update

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -12,13 +12,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Post('nest')
-  async nest(@Req() request: Request){
-    const server = new RabbitmqServer('amqp://admin:admin@rabbitmq:5672')
-    await server.start();
-    await server.publishInQueue('express', JSON.stringify(request.body));
-    await server.publishInExchange('amq.direct','rota2', JSON.stringify(request.body));
-    return request.body
-  }
 }

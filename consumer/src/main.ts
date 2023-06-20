6f17f710c007c6d41901ed8ca33b697cdcd05fa6
Ericksulino <ericksulino@gmail.com>
2023-06-20 19:23:12 -0300

    update

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import RabbitmqServer from './rabbitmq-server';
-import fetch from 'node-fetch';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,24 +8,8 @@ async function bootstrap() {
 
   const server = new RabbitmqServer('amqp://admin:admin@rabbitmq:5672');
   await server.start();
-  await server.consume('list',async (message) =>
-  {
-    const buffer: Buffer = message.content;
-    const bufferString: string = buffer.toString('utf-8');
-    const objetoJson = JSON.parse(bufferString);
-    const cep: string = objetoJson.cep;
-    console.log(cep)
-    try {
-      const response = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-      } else {
-        console.log('Erro na requisição:', response.status);
-      }
-    } catch (error) {
-      console.log('Erro na requisição:', error);
-    }
-  });
+  await server.consume('list', message =>
+    console.log(message.content.toString()),
+  );
 }
 bootstrap();
---
+++ main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import RabbitmqServer from './rabbitmq-server';
+import fetch from 'node-fetch';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+
+  const server = new RabbitmqServer('amqp://admin:admin@rabbitmq:5672');
+  await server.start();
+  await server.consume('list',async (message) =>
+  {
+    const buffer: Buffer = message.content;
+    const bufferString: string = buffer.toString('utf-8');
+    const objetoJson = JSON.parse(bufferString);
+    const cep: string = objetoJson.cep;
+    console.log(cep)
+    try {
+      const response = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        console.log('Erro na requisição:', response.status);
+      }
+    } catch (error) {
+      console.log('Erro na requisição:', error);
+    }
+  });
+}
+bootstrap();

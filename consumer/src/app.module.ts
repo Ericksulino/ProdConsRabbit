d9b32199de1465ef80e542463dee3cdd3f736f63
Ericksulino <ericksulino@gmail.com>
2023-06-21 09:12:06 -0300

    add mongo uri

--- consumer/src/app.module.ts
+++ consumer/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://root:example@mongo:27017/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })

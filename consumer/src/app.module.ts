1d8c762a5b8b2e828320bd12498a12b21e9fc5f9
Ericksulino <ericksulino@gmail.com>
2023-06-21 14:02:01 -0300

    update

--- consumer/src/app.module.ts
+++ consumer/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [],
